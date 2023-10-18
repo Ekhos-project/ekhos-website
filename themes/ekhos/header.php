6006bc318a83470c614c0db87e5598a5b64273d4
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-10-18 10:44:50 +0200

    feat: navigation with menu section

--- themes/ekhos/header.php
+++ themes/ekhos/header.php
@@ -6,5 +6,11 @@
 <body <?php body_class(); ?>>
 
 <nav class="navigation">
-
+    <?php
+    wp_nav_menu(array(
+        'theme_location' => 'header-menu',
+        'container' => false,
+        'items_wrap' => '<ul>%3$s</ul>',
+    ));
+    ?>
 </nav>
