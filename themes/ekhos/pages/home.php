f85859c29bfb8324514293e7e61430c2526e1a9c
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-10-18 10:01:15 +0200

    feat: identity section

--- themes/ekhos/pages/home.php
+++ themes/ekhos/pages/home.php
@@ -9,6 +9,97 @@
     <button>Découvrir</button>
 </section>
 
-<section class="section section-video" id="video">
+<section class="section section-home" id="video">
     <video muted autoplay loop src=""></video>
+</section>
+
+<section class="section section-home" id="identity">
+    <h2>Votre identité à vous</h2>
+    <p>EKHOS vous accompagne pour créer votre propre identité sonore et vocale</p>
+
+    <div class="identity_cards">
+        <div class="identity_card">
+            <div class="identity_card_icon">
+                <img src="" alt="">
+            </div>
+            <div class="identity_card_title">
+                <h3>Expérience Immersive Inégalée</h3>
+            </div>
+            <div class="identity_card_description">
+                <p>
+                    Offrez à vos utilisateurs une immersion sensorielle exceptionnelle qui transcende les simples visuels grâce à des interactions et effets sonores captivants.
+                </p>
+            </div>
+        </div>
+
+        <div class="identity_card">
+            <div class="identity_card_icon">
+                <img src="" alt="">
+            </div>
+            <div class="identity_card_title">
+                <h3>Différenciation Instantanée</h3>
+            </div>
+            <div class="identity_card_description">
+                <p>
+                    C’est l’opportunité de se démarquer de la concurrence en renforçant votre identité de marque et en créant une expérience unique pour les utilisateurs.
+                </p>
+            </div>
+        </div>
+
+        <div class="identity_card">
+            <div class="identity_card_icon">
+                <img src="" alt="">
+            </div>
+            <div class="identity_card_title">
+                <h3>Optimisation des Taux de Conversion</h3>
+            </div>
+            <div class="identity_card_description">
+                <p>
+                    Utilisez le websound pour influencer positivement le comportement des visiteurs, renforçant ainsi la confiance et favorisant l’action.
+                </p>
+            </div>
+        </div>
+
+        <div class="identity_card">
+            <div class="identity_card_icon">
+                <img src="" alt="">
+            </div>
+            <div class="identity_card_title">
+                <h3>Personnalisation Sans Limite</h3>
+            </div>
+            <div class="identity_card_description">
+                <p>
+                    Personnalisez entièrement l’expérience sonore de votre site web en choisissant des musiques, des effets sonores et des ambiances qui correspondent à votre identité de marque.
+                </p>
+            </div>
+        </div>
+
+        <div class="identity_card">
+            <div class="identity_card_icon">
+                <img src="" alt="">
+            </div>
+            <div class="identity_card_title">
+                <h3>Compatibilité Multiplateforme</h3>
+            </div>
+            <div class="identity_card_description">
+                <p>
+                    Notre plugin s’intègre harmonieusement sur diverses plateformes afin d’offrir une expérience audio exceptionnelle sur tous les supports.
+                </p>
+            </div>
+        </div>
+
+        <div class="identity_card">
+            <div class="identity_card_icon">
+                <img src="" alt="">
+            </div>
+            <div class="identity_card_title">
+                <h3>Analytique Audio</h3>
+            </div>
+            <div class="identity_card_description">
+                <p>
+                    Mesurez l’impact de l’expérience sonore avec des outils d’analyse avancés, identifiant les points forts et les domaines à améliorer pour maximiser l’engagement utilisateur.
+                </p>
+            </div>
+        </div>
+    </div>
 </section>
