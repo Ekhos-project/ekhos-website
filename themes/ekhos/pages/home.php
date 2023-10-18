d0120b693cc95b7411b5170b5a60af7c5c4958d7
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-10-18 09:53:11 +0200

    feat: home section

--- themes/ekhos/pages/home.php
+++ themes/ekhos/pages/home.php
@@ -1,1 +1,10 @@
 <?php ?>
+
+<section class="section section-home" id="hero">
+    <h1>L’AVENIR AUDIO DES SITES <b>E-COMMERCE</b></h1>
+
+    <p>Découvrez IDSound, le plug-in ultime de sound design qui
+        propulse vos sites e-commerce vers de nouveaux sommets</p>
+
+    <button>Découvrir</button>
+</section>
