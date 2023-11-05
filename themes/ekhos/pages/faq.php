4c741c11cd9fbee7f32533a24928ee07aeb66a5e
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-11-05 20:18:44 +0100

    feat: faq page & faq title section style

---
+++ themes/ekhos/pages/faq.php
@@ -0,0 +1,33 @@
+<?php ?>
+
+<section class="section section-faq" id="title">
+    <div class="faq_container">
+        <div class="faq_title">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/faq_title_icon.png" alt="faq">
+            <h1>Foire aux questions</h1>
+            <p>Trouvez des réponses à vos questions.</p>
+        </div>
+    </div>
+</section>
+
+<section class="section section-faq" id="questions">
+    <div class="questions_container">
+        <div class="questions_content">
+            <div class="questions_items">
+                <div class="questions_item">
+                    <div class="questions_item_title">
+                        <span>
+                            Des questions sur la compatibilité de notre plugin avec votre plateforme ? Besoin d'une démonstration personnalisée ? Contactez notre équipe de support technique.
+                        </span>
+                    </div>
+
+                    <div class="questions_item_response">
+                        <p>
+                            Des questions sur la compatibilité de notre plugin avec votre plateforme ? Besoin d'une démonstration personnalisée ? Contactez notre équipe de support technique.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
