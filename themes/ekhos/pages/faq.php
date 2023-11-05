7c87e0c1ef9c16853b25374f2dc8bf987d003cf9
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-11-05 22:04:25 +0100

    feat: faq query questions

--- themes/ekhos/pages/faq.php
+++ themes/ekhos/pages/faq.php
@@ -14,35 +14,33 @@
     <div class="questions_container">
         <div class="questions_content">
             <div class="questions_items">
-                <div class="questions_item">
-                    <div class="questions_item_expand"></div>
-                    <div class="questions_item_title">
-                        <span>
-                            Des questions sur la compatibilité de notre plugin avec votre plateforme ? Besoin d'une démonstration personnalisée ? Contactez notre équipe de support technique.
-                        </span>
-                    </div>
-
-                    <div class="questions_item_response">
-                        <p>
-                            Des questions sur la compatibilité de notre plugin avec votre plateforme ? Besoin d'une démonstration personnalisée ? Contactez notre équipe de support technique.
-                        </p>
-                    </div>
-                </div>
-
-                <div class="questions_item">
-                    <div class="questions_item_expand"></div>
-                    <div class="questions_item_title">
-                        <span>
-                            Des questions sur la compatibilité de notre plugin avec votre plateforme ? Besoin d'une démonstration personnalisée ? Contactez notre équipe de support technique.
-                        </span>
-                    </div>
-
-                    <div class="questions_item_response">
-                        <p>
-                            Des questions sur la compatibilité de notre plugin avec votre plateforme ? Besoin d'une démonstration personnalisée ? Contactez notre équipe de support technique.
-                        </p>
-                    </div>
-                </div>
+                <?php
+                $args = array(
+                    'post_type' => 'questions',
+                    'posts_per_page' => -1,
+                );
+                $query = new WP_Query($args);
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        $title = get_the_title();
+                        $reponse = get_field('reponse');
+                        $mise_en_avant = get_field('avant');
+                        ?>
+                        <div class="questions_item">
+                            <div class="questions_item_expand"></div>
+                            <div class="questions_item_title">
+                                <span><?= $title ?></span>
+                            </div>
+                            <div class="questions_item_response">
+                                <p><?= $reponse ?></p>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                }
+                wp_reset_postdata();
+                ?>
             </div>
         </div>
     </div>
