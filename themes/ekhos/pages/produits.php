7537480b1c132b033a789d51fa8234779208aa92
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-11-07 21:04:05 +0100

    feat: produits details section

--- themes/ekhos/pages/produits.php
+++ themes/ekhos/pages/produits.php
@@ -5,7 +5,8 @@
         <div class="title_title">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/produits_title_icon.png" alt="produits">
             <h1>IdSound</h1>
-            <p>IdSound vous offre un arsenal d’outils puissants pour façonner et transformer votre site e-commerce en <b>une histoire sonore captivante</b>.</p>
+            <p>IdSound vous offre un arsenal d’outils puissants pour façonner et transformer votre site e-commerce en
+                <b>une histoire sonore captivante</b>.</p>
         </div>
         <div class="title_actions">
             <a href="" class="button endicon">Essayer</a>
@@ -20,9 +21,12 @@
                 <h2>Presets Inspirants pour Votre Sound Design</h2>
             </div>
             <div class="description_description_text">
-                <p>Notre bibliothèque de presets soigneusement conçus vous offre un point de départ inspirant pour vos projets de sound design.
-                    </p>
-                <p>Des atmosphères envoûtantes aux effets percutants, en passant par les textures expérimentales, nos presets sont conçus pour vous aider à explorer de nouvelles sonorités et à stimuler votre inspiration.
+                <p>Notre bibliothèque de presets soigneusement conçus vous offre un point de départ inspirant pour vos
+                    projets de sound design.
+                </p>
+                <p>Des atmosphères envoûtantes aux effets percutants, en passant par les textures expérimentales, nos
+                    presets sont conçus pour vous aider à explorer de nouvelles sonorités et à stimuler votre
+                    inspiration.
             </div>
             <div class="description_description_action">
                 <a href="" class="button endicon">Essayer</a>
@@ -32,6 +36,29 @@
         <div class="description_visual">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/produits_description_visual.png"
                  alt="IdSound">
+        </div>
+    </div>
+</section>
+
+<section class="section section-produits" id="details">
+    <div class="details_container">
+        <div class="details_visual">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/produits_details_image.png" alt="details">
+        </div>
+        <div class="details_description">
+            <div class="details_description_title">
+                <h2>IdSound détails</h2>
+            </div>
+            <div class="details_description_text">
+                <p>Enrichissez vos projets en ligne avec des expériences audio captivantes et personnalisées et accédez
+                    à des outils afin d'analyser et améliorer l'expérience de vos utilisateurs.</p>
+            </div>
+            <div class="details_description_adventage">
+                <span>Bibliothèque Audio Étendue</span>
+                <span>Synchronisation Intelligente</span>
+                <span>Analyse Audio Avancée</span>
+                <span>Intégration Multicanal</span>
+            </div>
         </div>
     </div>
 </section>
