87ba234037326bcaa20b20c554ffabf947e04f1f
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-11-07 19:55:21 +0100

    feat: produits title section

---
+++ themes/ekhos/pages/produits.php
@@ -0,0 +1,14 @@
+<?php ?>
+
+<section class="section section-produits" id="title">
+    <div class="title_container">
+        <div class="title_title">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/produits_title_icon.png" alt="produits">
+            <h1>IdSound</h1>
+            <p>IdSound vous offre un arsenal d’outils puissants pour façonner et transformer votre site e-commerce en <b>une histoire sonore captivante</b>.</p>
+        </div>
+        <div class="title_actions">
+            <a href="" class="button endicon">Essayer</a>
+        </div>
+    </div>
+</section>
