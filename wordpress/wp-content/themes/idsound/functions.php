fb8916f60d13a4e676fb5b794e80d040a7089f0d
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2024-03-24 18:21:38 +0100

    feat: defaults menus

--- wordpress/wp-content/themes/idsound/functions.php
+++ wordpress/wp-content/themes/idsound/functions.php
@@ -27,7 +27,6 @@ function disable_email_login( $user, $username, $password ) {
     if ( is_email( $username ) ) {
         return new WP_Error( 'disabled_email_login', __("<strong>ERREUR</strong> : La connexion par email est désactivée. Veuillez utiliser votre nom d'utilisateur.") );
     }
-
     return $user;
 }
 add_filter( 'authenticate', 'disable_email_login', 20, 3 );
@@ -48,6 +47,95 @@ function create_page_if_not_exists($page_slug, $page_title) {
     }
 }
 
+function create_and_assign_menu($menu_name, $menu_location) {
+    $menu_exists = wp_get_nav_menu_object($menu_name);
+    if (!$menu_exists) {
+        $menu_id = wp_create_nav_menu($menu_name);
+    } else {
+        $menu_id = $menu_exists->term_id;
+    }
+    $locations = get_theme_mod('nav_menu_locations');
+    $locations[$menu_location] = $menu_id;
+    set_theme_mod('nav_menu_locations', $locations);
+}
+
+create_and_assign_menu('Menu header', 'header-menu');
+create_and_assign_menu('Menu footer', 'footer-menu');
+create_and_assign_menu('Menu Legals', 'legals-menu');
+
+
+function add_page_to_menu($menu_name, $page_slug, $page_title = null, $classes_array = array(), $parent_id = 0){
+    // Tenter de récupérer le menu par son slug
+    $menu_obj = wp_get_nav_menu_object($menu_name);
+
+    // Si le menu n'existe pas, le créer
+    if (!$menu_obj) {
+        return;
+    }
+
+    $menu_id = $menu_obj->term_id;
+
+    // Trouver la page par son slug
+    $page = get_page_by_path($page_slug);
+    if (!$page) {
+        echo "Page '$page_slug' non trouvée.";
+        return;
+    }
+    $page_id = $page->ID;
+
+    // Vérifier si la page est déjà dans le menu
+    $menu_items = wp_get_nav_menu_items($menu_id);
+    foreach ($menu_items as $menu_item) {
+        if ($menu_item->object_id == $page_id) {
+            echo "Page déjà présente dans le menu.";
+            return;
+        }
+    }
+
+    // Ajouter la page au menu
+    $result = wp_update_nav_menu_item($menu_id, 0, array(
+        'menu-item-title' => $page_title ? $page_title : $page->post_title,
+        'menu-item-object-id' => $page_id,
+        'menu-item-object' => 'page',
+        'menu-item-type' => 'post_type',
+        'menu-item-status' => 'publish',
+        'menu-item-classes' => implode(' ', $classes_array),
+        'menu-item-parent-id' => $parent_id,
+    ));
+
+    if (is_wp_error($result)) {
+        echo "Erreur lors de l'ajout de la page au menu.";
+    } else {
+        echo "Page '$page_slug' ajoutée avec succès au menu '$menu_slug'.";
+    }
+
+    return $result;
+}
+
+
+function create_menu_item($menu_name, $title, $url = '', $parent_id = 0) {
+    $menu_obj = wp_get_nav_menu_object($menu_name);
+    if (!$menu_obj) {
+        return;
+    }
+    $menu_id = $menu_obj->term_id;
+
+    $menu_items = wp_get_nav_menu_items($menu_id);
+    foreach ($menu_items as $menu_item) {
+        if ($menu_item->title == $title) {
+            return $menu_item->ID;
+        }
+    }
+    return wp_update_nav_menu_item($menu_id, 0, array(
+        'menu-item-title' => $title,
+        'menu-item-url' => $url,
+        'menu-item-status' => 'publish',
+        'menu-item-parent-id' => $parent_id,
+        'menu-item-type' => 'custom',
+    ));
+}
+
+
 function set_homepage_by_slug($page_slug) {
     $page = get_page_by_path($page_slug);
 
@@ -62,6 +150,7 @@ create_page_if_not_exists('accueil', 'Accueil');
 create_page_if_not_exists('blog', 'Blog');
 create_page_if_not_exists('contact', 'Contact');
 create_page_if_not_exists('cookies', 'Cookies');
+create_page_if_not_exists('demo', 'Demo');
 create_page_if_not_exists('faq', 'FAQ');
 create_page_if_not_exists('plan-du-site', 'Plan du site');
 create_page_if_not_exists('politique-de-confidentialite', 'Politique de confidentialité');
@@ -69,3 +158,10 @@ create_page_if_not_exists('prix', 'Prix');
 create_page_if_not_exists('produits', 'Produits');
 create_page_if_not_exists('v1', 'V1');
 set_homepage_by_slug('accueil');
+add_page_to_menu('Menu header', 'produits');
+$ressources = create_menu_item('Menu header', 'Ressources', '#ressources');
+add_page_to_menu('Menu header', 'blog', null, array(), $ressources);
+add_page_to_menu('Menu header', 'faq', null, array(), $ressources);
+add_page_to_menu('Menu header', 'prix');
+add_page_to_menu('Menu header', 'contact');
+add_page_to_menu('Menu header', 'demo', 'Essayer', array('demo'));
